ddf7b21e2b68a83960be5c0c857b47f331372d9a
Ailton Duarte <adidas.coyas@gmail.com>
2024-04-21 18:39:34 -0100

    feat: new configs

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,26 @@
-module.exports = () => ({});
+module.exports = () => ({
+    "strapi-plugin-populate-deep": {
+        config: {
+          defaultDepth: 3, // Default is 5
+        },
+      },
+      // ckeditor: true,
+      ckeditor5: {
+        enabled: true,
+        // resolve: "./src/plugins/strapi-plugin-ckeditor"
+      },
+      menus: {
+        config: {
+          maxDepth: 3,
+        },
+    },
+    upload: {
+        config: {
+          providerOptions: {
+            localServer: {
+              maxage: 300000
+            },
+          },
+        },
+      },
+});
