7c04c95b11aefe6b3f54fa7e0d42921de05538fb
Ailton Duarte <adidas.coyas@gmail.com>
2024-12-28 18:52:49 -0100

    feat: add sendmail config to info@pnp.cv

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = () => ({
+module.exports = ({env}) => ({
     "strapi-plugin-populate-deep": {
         config: {
           defaultDepth: 3, // Default is 5
@@ -21,6 +21,19 @@ module.exports = () => ({
               maxage: 300000,
               sizeLimit: 1024 * 1024 * 1024 // 256mb in bytes
             },
+          },
+        },
+      },
+      email: {
+        config: {
+          provider: 'sendmail', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+          providerOptions: {
+           // apiKey: env('SENDGRID_API_KEY'),
+          },
+          settings: {
+            defaultFrom: 'info@pnp.cv',
+            defaultReplyTo: 'info@pnp.cv',
+            testAddress: 'info@pnp.cv',
           },
         },
       },
